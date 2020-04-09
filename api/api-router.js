2fe21430ac0f7507c16e399059bf7577d4e5c555
ranraok <46853842+ranraok@users.noreply.github.com>
2020-04-09 13:50:47 -0400

    updated again

--- api/api-router.js
+++ api/api-router.js
@@ -7,7 +7,8 @@ const router = express.Router();
 router.use(express.json());
 
 router.get("/", (req, res) => {
-  res.status(200).json({ api: "up" });
+  const message = process.env.MESSAGE || 'HELLO FROM HEROKU';
+  res.status(200).json({ api: "up", message });
 });
 
 router.get("/shouts", (req, res, next) => {
